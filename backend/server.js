c9f8ccd5575e96f41afd3f2acd39b928dff59f77
Emin Bayrak <eminb@outlook.com>
2020-12-16 23:12:44 -0500

    server changes

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,7 @@ app.get('*', (request, response) => {
 app.use(bodyParser.urlencoded({ extended: false }))
 // Parse application/json
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 // Middleware routes
 app.use('/api', authRoute);
 app.use(cors());
